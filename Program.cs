b40d58ab336a6c1e0cd33c48520e2b718a3b783c
Markus-Lundgren <markus.lundgren.99@gmail.com>
2026-09-15 15:24:44 +0200

    feat: add Book class

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+	static void Main()
+	{
+		Book book1 = new Book("Dune", "Frank Herbert");
+		Book book2 = new Book("Frankenstein", "Mary Shelly");
+	}
+}
+
+class Book
+{
+	private string title;
+	private string author;
+	public Book(string bookTitle, string bookAuthor)
+	{
+		title = MInput.CheckInput(bookTitle);
+		author = MInput.CheckInput(bookAuthor);
+	}
+
+}
